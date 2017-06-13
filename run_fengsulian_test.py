dbb2cc17361cc5468872f293ef3f5f9d5a5c495c
xyf186 <790577657@qq.com>
2017-06-13 21:35:20 +0800

    登录用例，数据驱动DDT

---
+++ run_fengsulian_test.py
@@ -0,0 +1,60 @@
+import HTMLTestRunner,time,os,unittest
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def sendemail(new_file):
+    _from  = "790577657@qq.com"
+    _pwd   = "qwmzgnsieatxbbei"
+    _to    = "1292027762@qq.com"
+    with open(filename,"rb") as f:
+        _body = f.read()
+    
+    msg = MIMEText(_body,"html","utf-8")
+    msg["Subject"] = "自动化测试报告_风速链登录"
+    msg["date"] = time.strftime("%a, %d %b %Y %H:%M:%S %z")
+    msg["From"] = _from
+    msg["To"] = _to
+    try:
+      smtp = smtplib.SMTP_SSL("smtp.qq.com",465)
+      smtp.login(_from,_pwd)
+      smtp.sendmail(_from,_to,msg.as_string())
+      smtp.quit()
+      print("Mail sends successfully！")
+    except smtplib.SMTPException as e:
+      print("Mail sending failed,the reason is %s" % e)
+
+def findreport():
+    result_dir = r"F:\selenium_files\fengsulian\report"
+    lists = os.listdir(result_dir)
+    lists.sort(key=lambda fn: os.path.getmtime(result_dir+ "\\" +fn))
+           #if not os.path.isdir(result_dir+ "\\" +fn) else 0)
+    print('The latest test_file is ：' + lists[-1])
+    new_file = os.path.join(result_dir,lists[-1])
+    print(new_file)
+    sendemail(new_file)
+
+
+
+
+
+if __name__ == "__main__":
+    now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime())
+    filename = ("./report/"+now+'result.html')
+    fp = open(filename,'wb')
+
+    runner = HTMLTestRunner.HTMLTestRunner(
+    stream=fp,
+    title = u'微博登录测试报告',
+    description=u'用例执行情况')
+    discover = unittest.defaultTestLoader.discover('./test_case',
+                                                   pattern='*_sta.py')
+
+
+
+    runner.run(discover)
+    fp.close()
+
+    findreport()
---
+++ test_case/page_obj/loginPage.py
@@ -0,0 +1,88 @@
+from .base import *
+from time import  sleep
+from selenium import webdriver
+
+url= "http://www.fengsulian.com/"
+
+class LoginPage(Page):
+    '''
+    用户登录
+    '''
+
+    #定位器，定位页面元素
+
+    #登录框
+    login_alert = ("css selector",".notLogin>a")
+    #用户名
+    username_loc = ("id","username")
+    #密码
+    pwd_loc = ("id","password")
+    #登录按钮
+    login_button = ("xpath",".//input[@value='登录']")
+
+    #用户名为空
+    username_null_loc = ("xpath","html/body/div[4]")
+    #密码为空
+    pwd_null_loc = ("xpath","html/body/div[4]")
+    #用户名、密码错误
+    username_err_loc = ("xpath","html/body/div[4]")
+
+    def __init__(self):
+        self.driver = browser('chrome')
+
+    def loginalert(self):
+        self.click(self.login_alert)
+
+    def input_username(self,username):
+        '''
+        输入帐号 
+        '''
+        self.send_keys(self.username_loc,username)
+
+    def input_pwd(self,pwd):
+        '''
+        输入密码
+        '''
+        self.send_keys(self.pwd_loc,pwd)
+
+    def login_submit(self):
+        '''
+        点击登录按钮
+        '''
+        self.click(self.login_button)
+
+    #登录方法
+    def login(self,username,pwd):
+        '''
+        登录方法  
+        '''
+        self.loginalert()
+        self.input_username(username)
+        self.input_pwd(pwd)
+        self.login_submit()
+
+    def is_login_sucess(self):
+        '''
+        判断是否获取到登录名称
+        '''
+        try:
+            text = self.find_element("css selector",".loginIn.fl>span").text
+            return True
+        except:
+            return False
+
+
+
+'''
+    #用户名为空提醒
+    def uesrname_null(self):
+        return self.find_element(self.username_null_loc).text
+    
+    #密码为空提醒
+    def pwd_null(self):
+        return self.find_element(self.pwd_null_loc).text
+    
+    #帐号、密码错误
+    def username_err(self):
+        return self.find_element(self.username_err_loc).text
+    '''
